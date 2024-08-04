841091a5004ae7319b4e8f44baec7b1ea05fe9fa
Natarajan-rangasamy <natraj2431@gmail.com>
2024-08-04 08:11:21 +0530

    added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number you wanna check: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("It's an Even");
+            }
+            else
+            {
+                Console.WriteLine("It's an Odd");
+            }
+
+
+            }
+    }
+}
